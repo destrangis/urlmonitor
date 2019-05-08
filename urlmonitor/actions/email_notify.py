79ee7eb4858c7a38b85a6fe13848217f7ed9751a
Javier Llopis <javier@llopis.me>
2019-05-08 08:32:43 +0100

    Extend the email_notify action
    Support for SMTP encryption via SSL or STARTTLS
    Support for user specified port
    Support for SMTP authentication

--- urlmonitor/actions/email_notify.py
+++ urlmonitor/actions/email_notify.py
@@ -1,8 +1,11 @@
+import base64
 import platform
 import smtplib
 from email.message import EmailMessage
 
 from urlmonitor.actionbase import Action
+
+_NODE = platform.node()
 
 _msg_pattern = """
 The Python UrlMonitor application running on {host} is happy to
@@ -26,8 +29,15 @@ BTW Do not reply to this message -- it will not compute.
 
 class _EmailAction(Action):
 
-    check_cfg_vars = [ "smtp_server" ]
-    default_vars = {}
+    check_cfg_vars = [ "smtp_server", "from_address", "smtp_encryption",
+            "smtp_user", "smtp_password", "smtp_port"]
+    default_vars = {
+        "from_address": "urlmonitor@{}".format(_NODE),
+        "smtp_encryption": None,
+        "smtp_user": None,
+        "smtp_password": "",
+        "smtp_port": 0,
+        }
 
     def make_message(self, fromaddr, toaddr, subject, text):
         msg = EmailMessage()
@@ -39,17 +49,40 @@ class _EmailAction(Action):
 
 
     def send_msg(self, server, fromaddr, toaddr, msg):
-        with smtplib.SMTP(server) as s:
-            s.send_message(msg)
+        if self.smtp_port == 0:
+            if self.smtp_encryption is None:
+                port = 25
+            else:
+                port = 587
+        else:
+            port = self.smtp_port
+
+        if self.smtp_encryption == "SSL":
+            srv = smtplib.SMTP_SSL(server, port)
+        else:
+            srv = smtplib.SMTP(server, port)
+
+        with srv:
+            if self.smtp_encryption == "STARTTLS":
+                srv.starttls()
+
+            if self.smtp_user is not None:
+                passwd = base64.b64decode(self.smtp_password).decode()
+                srv.login(self.smtp_user, passwd)
+            srv.send_message(msg)
 
 
     def __call__(self, name, arglst, url, content, variables, log):
-        hostname = platform.node()
+        hostname = _NODE
         fromaddr = "UrlMonitor Bot <noreply@destrangis.com>"
         subject = "URL Changed: " + url
         contents = _msg_pattern.format(url=url, host=hostname, **variables)
         msg = self.make_message(fromaddr, arglst, subject, contents)
-        self.send_msg(self.smtp_server, fromaddr, arglst, msg)
+        try:
+            self.send_msg(self.smtp_server, fromaddr, arglst, msg)
+        except smtplib.SMTPException as exc:
+            log.error("{}: {}".format(name, exc))
+
 
 
 action_object = _EmailAction()
